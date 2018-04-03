ce9a493b383b714df1c55744e33a869eb7e57465
Tobias Dammers <tdammers@gmail.com>
2018-04-03 15:42:53 +0200

    JS refactor

---
+++ client/src/components/vornav.js
@@ -0,0 +1,58 @@
+var R = require('ramda')
+var templates = require('../templates')
+var $ = require('jquery')
+
+var create = function() {
+    var container = $('<div>')
+    var spinnerOverlay = $(templates.spinner.spinner())
+    $(container).append(spinnerOverlay.hide())
+
+    var vornavForm = $(templates.vornav.form({}))
+    $(container).append(vornavForm)
+
+    vornavForm.on('submit', function(e) {
+        var form = $(this)
+        var from = $('[name=from]', this).val()
+        var to = $('[name=to]', this).val()
+        var resultbox = $('.result', form)
+        spinnerOverlay.fadeIn()
+        resultbox.fadeOut()
+
+        var success = function(data) {
+            resultbox.empty()
+            resultbox.append($(templates.vornav.result(data)).children())
+            resultbox.fadeIn()
+        }
+
+        var failure = function(data) {
+            resultbox.empty()
+            resultbox.text(data.responseText)
+            resultbox.fadeIn()
+        }
+
+        $.get(
+            '/api/vornav/' +
+            encodeURIComponent(from) +
+            '/' +
+            encodeURIComponent(to))
+            .done(success)
+            .fail(failure)
+            .always(function(){ spinnerOverlay.fadeOut() })
+        e.preventDefault()
+        return false
+    })
+
+    return {
+        hide: function() {
+            container.hide()
+        },
+        show: function() {
+            container.show()
+        },
+        elem: container
+    }
+}
+
+module.exports =
+    { create: create
+    }
--- client/src/main.js
+++ client/src/main.js
@@ -1,46 +1,11 @@
 var $ = require('jquery')
 var R = require('ramda')
 var h = require('hyperscript')
-var templates = require('./templates')
+var components = {
+        vornav: require('./components/vornav')
+    }
 
-var prepareVornavForm = function() {
-    var spinnerOverlay = $(templates.spinner.spinner())
-    $('body').append(spinnerOverlay.hide())
-
-    var vornavForm = $(templates.vornav.form({}))
-    $('body').append(vornavForm)
-
-    vornavForm.on('submit', function(e) {
-        var form = $(this)
-        var from = $('[name=from]', this).val()
-        var to = $('[name=to]', this).val()
-        var resultbox = $('.result', form)
-        spinnerOverlay.fadeIn()
-        resultbox.fadeOut()
-
-        var success = function(data) {
-            resultbox.empty()
-            resultbox.append($(templates.vornav.result(data)).children())
-            resultbox.fadeIn()
-        }
-
-        var failure = function(data) {
-            resultbox.empty()
-            resultbox.text(data.responseText)
-            resultbox.fadeIn()
-        }
-
-        $.get(
-            '/api/vornav/' +
-            encodeURIComponent(from) +
-            '/' +
-            encodeURIComponent(to))
-            .done(success)
-            .fail(failure)
-            .always(function(){ spinnerOverlay.fadeOut() })
-        e.preventDefault()
-        return false
-    })
-}
-
-$(document).ready(prepareVornavForm)
+$(document).ready(function() {
+    var vornav = components.vornav.create()
+    $('body').append($(vornav.elem))
+})
