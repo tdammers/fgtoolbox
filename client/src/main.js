855cef59122abb1b1a5d4840469e7c64efedefbe
Tobias Dammers <tdammers@gmail.com>
2018-04-03 12:52:14 +0200

    Fix AJAX error handling, clean up NPM package

--- client/src/main.js
+++ client/src/main.js
@@ -19,42 +19,51 @@ $(document).ready(function(){
         var to = $('[name=to]', this).val()
         var spinnerOverlay = expandTemplate('#tplSpinner')
         form.append(spinnerOverlay)
+        var resultbox = $('.result', form)
+
+        var success = function(data) {
+            resultbox.empty()
+            resultbox
+                .append(
+                    expandTemplate('#tplVornavResult',
+                        {
+                            'from-id': data.from.id,
+                            'from-name': data.from.name,
+                            'to-id': data.to.id,
+                            'to-name': data.to.name
+                        }))
+            resultbox
+                .append(
+                    $('<h4>')
+                        .text(
+                            data.dist.toFixed(1) +
+                            'nm'))
+            R.forEach(
+                function(waypoint){
+                    resultbox
+                        .append(
+                            $('<div>')
+                                .append(
+                                    $('<strong>')
+                                        .text(waypoint.id)))
+                },
+                data.waypoints)
+            console.log(data)
+        }
+
+        var failure = function(data) {
+            resultbox.empty()
+            resultbox.text(data.responseText)
+        }
+
         $.get(
             '/api/vornav/' +
             encodeURIComponent(from) +
             '/' +
-            encodeURIComponent(to),
-            function(data) {
-                var resultbox = $('.result', form)
-                spinnerOverlay.remove()
-                resultbox.empty()
-                resultbox
-                    .append(
-                        expandTemplate('#tplVornavResult',
-                            {
-                                'from-id': data.from.id,
-                                'from-name': data.from.name,
-                                'to-id': data.to.id,
-                                'to-name': data.to.name
-                            }))
-                resultbox
-                    .append(
-                        $('<h4>')
-                            .text(
-                                data.dist.toFixed(1) +
-                                'nm'))
-                R.forEach(
-                    function(waypoint){
-                        resultbox
-                            .append(
-                                $('<div>')
-                                    .append(
-                                        $('<strong>')
-                                            .text(waypoint.id)))
-                    },
-                    data.waypoints)
-                console.log(data)
-            })
+            encodeURIComponent(to))
+            .done(success)
+            .fail(failure)
+            .always(function(){ spinnerOverlay.remove() })
         e.preventDefault()
         return false
     })
